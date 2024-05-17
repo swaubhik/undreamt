bafface959c9dac85aafb49fa138a4b14eea1924
swaubhik <p22cse100@cit.ac.in>
2024-05-17 05:35:03 +0530

    chore: Convert 0-dim tensor to a number in Trainer class

--- undreamt/train.py
+++ undreamt/train.py
@@ -334,7 +334,7 @@ class Trainer:
         # Compute loss
         t = time.time()
         loss = self.translator.score(src, trg, train=True)
-        self.loss += loss.data[0]
+        self.loss += loss.item()  # Fix: Convert 0-dim tensor to a number
         self.forward_time += time.time() - t
 
         # Backpropagate error + optimize
